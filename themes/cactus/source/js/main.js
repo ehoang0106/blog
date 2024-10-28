42a73da0e1412ef81b91da80df78fceb961f4b0c
Khoa Hoang <khoahoang1698@gmail.com>
2024-10-28 15:15:19 -0700

    fix nav bar does not show after scroll down

--- themes/cactus/source/js/main.js
+++ themes/cactus/source/js/main.js
@@ -62,7 +62,7 @@ $(document).ready(function() {
         if (!nav.is(":visible") && topDistance < 50) {
           nav.show();
         } else if (nav.is(":visible") && topDistance > 100) {
-          nav.hide();
+          nav.show();
         }
 
         // on tablet, hide the navigation icon as well and show a "scroll to top
